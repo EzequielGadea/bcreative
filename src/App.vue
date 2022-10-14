a2896a7afb85838b6225786853e8a57834086c77
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-13 22:00:21 -0300

    tailwind, dependencias y eliminaciones

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
+  <h1 class="text-3xl font-bold underline">
+    HOLA MUNDO
+  </h1>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
